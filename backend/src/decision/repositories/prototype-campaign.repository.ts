40e1a527e158837546c7a22ec74e7e3f8aed8efd
tomass <dominanthat@gmail.com>
2025-12-16 22:09:03 +0900

    feat: campaign 레포 인터페이스 정의 및 구현
    - campagin.repository.interface.ts: campaign 레포 인터페이스 정의(비동기 반환 및 추상 클래스 사용)
    - prototype-campagin.repository.ts: capmpagin 레포 구현체 구현

---
+++ backend/src/decision/repositories/prototype-campaign.repository.ts
@@ -0,0 +1,36 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { CampaignRepository } from './campaign.repository.interface';
+import { Campaign, Tag } from '../types/decision.types';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+@Injectable()
+export class InMemoryCampaignRepository
+  implements CampaignRepository, OnModuleInit
+{
+  private campaigns: Campaign[] = [];
+
+  async onModuleInit() {
+    // 서버 시작 시 한 번만 파일을 비동기로 읽어 메모리에 적재 -> 나중에 Redis로 전환 필요
+    const filePath = join(process.cwd(), 'src/data/campaigns.mock.json');
+    const fileContent = await readFile(filePath, 'utf-8');
+    this.campaigns = JSON.parse(fileContent) as Campaign[];
+  }
+
+  findByTags(tags: Tag[]): Promise<Campaign[]> {
+    const tagNames = tags.map((tag) => tag.name);
+    return Promise.resolve(
+      this.campaigns.filter((campaign) =>
+        campaign.tags.some((tag) => tagNames.includes(tag.name))
+      )
+    );
+  }
+
+  findById(id: string): Promise<Campaign | null> {
+    return Promise.resolve(this.campaigns.find((c) => c.id === id) || null);
+  }
+
+  findAll(): Promise<Campaign[]> {
+    return Promise.resolve(this.campaigns);
+  }
+}
