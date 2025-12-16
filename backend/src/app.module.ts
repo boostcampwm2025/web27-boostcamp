082992a5a995fef4ee8754edaa8a32c713778772
tomass <dominanthat@gmail.com>
2025-12-17 02:57:48 +0900

    feat: rtb 서비스 모듈에 등록

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RTBModule } from './rtb/rtb.module';
 
 @Module({
-  imports: [],
+  imports: [RTBModule],
   controllers: [AppController],
   providers: [AppService],
 })
