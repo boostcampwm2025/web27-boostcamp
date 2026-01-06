f2fc386a9bcd20354c106945d89bde843b9b2e42
chazicer <my0710my@gmail.com>
2026-01-07 06:36:16 +0900

    feat: 백엔드 폴더 마이그레이션
    - 백엔드 폴더 전체 가져오기

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RTBModule } from './rtb/rtb.module';
+import { ClickModule } from './click/click.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ClickModule, RTBModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // API Global Prefix 설정 (예: http://localhost:3000/api/...)
+  app.setGlobalPrefix('api');
+
   // CORS 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: [process.env.CORS_ORIGIN],
     credentials: true,
   });
 
