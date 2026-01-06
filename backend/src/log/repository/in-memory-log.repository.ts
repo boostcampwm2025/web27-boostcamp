99ffd7237f59d64460bfd360bbfefd94dc356663
kitae <gitae9999@naver.com>
2026-01-07 00:15:33 +0900

    feat: InMemoryRepository 클래스 구현

--- backend/src/log/repository/in-memory-log.repository.ts
+++ backend/src/log/repository/in-memory-log.repository.ts
@@ -1,3 +1,37 @@
+import { SaveClickLogDto } from '../dto/save-click-log.dto';
+import { SaveViewLogDto } from '../dto/save-view-log.dto';
 import { LogRepository } from './log.repository';
 
-export class InMemoryLogRepository implements LogRepository {}
+export class InMemoryLogRepository extends LogRepository {
+  private readonly viewLog = new Map<number, SaveViewLogDto>();
+  private viewLogIdx = 0;
+  private readonly clickLog = new Map<number, SaveClickLogDto>();
+  private clickLogIdx = 0;
+
+  // 인메모리 Map 자료구조에 저장 (다음주에 DB로 갈아끼우기)
+  saveViewLog(dto: SaveViewLogDto): number {
+    const { auctiondId, campaignId, blogId, cost, positionRatio, createdAt } =
+      dto;
+    this.viewLogIdx += 1;
+    this.viewLog.set(this.viewLogIdx, {
+      auctiondId,
+      campaignId,
+      blogId,
+      cost,
+      positionRatio,
+      createdAt,
+    });
+
+    return this.viewLogIdx;
+  }
+
+  // todo: abstact class 선언해두어서 일단 구현부를 작성해두었는데 return을 어떤걸 해줄지 정하고 수정할 예정
+  saveClickLog(dto: SaveClickLogDto): void {
+    const { viewId, createdAt } = dto;
+    this.clickLogIdx += 1;
+    this.clickLog.set(this.clickLogIdx, {
+      viewId,
+      createdAt,
+    });
+  }
+}
--- backend/src/log/repository/log.repository.ts
+++ backend/src/log/repository/log.repository.ts
@@ -2,6 +2,6 @@ import { SaveClickLogDto } from '../dto/save-click-log.dto';
 import { SaveViewLogDto } from '../dto/save-view-log.dto';
 
 export abstract class LogRepository {
-  abstract saveViewLog(dto: SaveViewLogDto): Promise<void>;
-  abstract saveClickLog(dto: SaveClickLogDto): Promise<void>;
+  abstract saveViewLog(dto: SaveViewLogDto): number;
+  abstract saveClickLog(dto: SaveClickLogDto): void;
 }
