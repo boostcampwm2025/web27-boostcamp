aa0d97f7d5022e1c013e42ee5c4e5f6efa3dbf88
kitae <gitae9999@naver.com>
2026-01-06 17:46:40 +0900

    chore: advertiser, sdk 모듈 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SdkModule } from './sdk/sdk.module';
+import { AdvertiserModule } from './advertiser/advertiser.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SdkModule,
+    AdvertiserModule,
+  ],
   controllers: [],
   providers: [],
 })
