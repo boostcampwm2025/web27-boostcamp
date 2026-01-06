a39e77dda9c4164905af81f224dccc5d22617e60
kitae <gitae9999@naver.com>
2026-01-06 22:23:16 +0900

    feat: SdkController Post /campaign-view 메서드 구현

--- backend/src/sdk/sdk.controller.ts
+++ backend/src/sdk/sdk.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { SdkService } from './sdk.service';
+import { CreateViewLogDto } from './dto/create-view-log.dto';
 
 @Controller('sdk')
 export class SdkController {
   constructor(private readonly sdkservice: SdkService) {}
+
+  @Post('campaign-view')
+  logCampaignView(@Body() createViewLogDto: CreateViewLogDto) {
+    this.sdkservice.logCampaignView(createViewLogDto);
+  }
 }
--- backend/src/sdk/sdk.service.ts
+++ backend/src/sdk/sdk.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { CreateViewLogDto } from './dto/create-view-log.dto';
 
 @Injectable()
-export class SdkService {}
+export class SdkService {
+  logCampaignView(dto: CreateViewLogDto) {
+    const { auctionId, campaginId, positionRatio } = dto;
+
+    
+  }
+}
