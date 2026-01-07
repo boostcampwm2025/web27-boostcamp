1977871bb723032c5789c4b4eb273f02536a9368
tomass <dominanthat@gmail.com>
2026-01-08 04:56:27 +0900

    feat: controller부분에 candiate 로깅 부분 추가

--- backend/src/rtb/rtb.controller.ts
+++ backend/src/rtb/rtb.controller.ts
@@ -6,8 +6,12 @@ import { RTBRequestDto } from './dto/rtb-request.dto';
 import { RTBResponseDto } from './dto/rtb-response.dto';
 import type { DecisionContext } from './types/decision.types';
 
+import { Logger } from '@nestjs/common';
+
 @Controller('sdk')
 export class RTBController {
+  private readonly logger = new Logger(RTBController.name);
+
   constructor(private readonly rtbService: RTBService) {}
 
   @Post('decision')
@@ -21,6 +25,17 @@ export class RTBController {
     };
 
     const result = await this.rtbService.runAuction(context);
+
+    result.data?.candidates?.forEach((candidate) => {
+      const eachCandidateLog = {
+        id: candidate.id,
+        title: candidate.title.slice(0, 10) + '...',
+        tags: candidate.tags,
+        score: candidate.score,
+      };
+
+      this.logger.log(JSON.stringify(eachCandidateLog));
+    });
 
     // Expose 데코레이터가 붙은 속성만 포함하여 DTO 인스턴스로 변환
     return plainToInstance(RTBResponseDto, result, {
