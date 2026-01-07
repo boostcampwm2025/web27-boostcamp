bb36a5c1b7e144f8f5da1f439ec219606cc1da23
kitae <gitae9999@naver.com>
2026-01-07 16:54:20 +0900

    feat: sdk module에 cache module import

--- backend/src/cache/cache.module.ts
+++ backend/src/cache/cache.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuctionStore } from './auction/auction.store';
+import { InMemoryAuctionStore } from './auction/in-memory-auction.store';
 
-@Module({})
+@Module({
+  providers: [{ provide: AuctionStore, useClass: InMemoryAuctionStore }],
+  exports: [AuctionStore],
+})
 export class CacheModule {}
--- backend/src/sdk/sdk.module.ts
+++ backend/src/sdk/sdk.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SdkController } from './sdk.controller';
 import { SdkService } from './sdk.service';
 import { LogModule } from 'src/log/log.module';
+import { CacheModule } from 'src/cache/cache.module';
 
 @Module({
-  imports: [LogModule],
+  imports: [LogModule, CacheModule],
   controllers: [SdkController],
   providers: [SdkService],
 })
