50babc1948eb8521c00a2a20feed5f9c07221884
kitae <gitae9999@naver.com>
2026-01-07 17:36:37 +0900

    chore: CI 실패 원인 수정
    - Jest가 import 경로 이해못하는 문제 수정 package.json:jest에 moduleNameMapper 추가
    - DI 에러 수정

--- backend/src/sdk/sdk.controller.spec.ts
+++ backend/src/sdk/sdk.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SdkController } from './sdk.controller';
+import { SdkService } from './sdk.service';
+import { LogRepository } from 'src/log/repository/log.repository';
+import { AuctionStore } from 'src/cache/auction/auction.store';
 
 describe('SdkController', () => {
   let controller: SdkController;
@@ -7,6 +10,24 @@ describe('SdkController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SdkController],
+      providers: [
+        SdkService,
+        {
+          provide: LogRepository,
+          useValue: {
+            saveViewLog: jest.fn(),
+            saveClickLog: jest.fn(),
+          },
+        },
+        {
+          provide: AuctionStore,
+          useValue: {
+            set: jest.fn(),
+            get: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<SdkController>(SdkController);
