44362d39523e2dd547a18082e627a214e01c60ca
2seb2 <yuo231@naver.com>
2026-01-07 18:20:11 +0900

    chore: TS 및 Vite alias / svgr 설정 추가
    - tsconfig.app.json에 경로 alias 설정 추가
    (@app, @features, @pages, @shared)
    - vite.config.ts에 alias 설정과 vite-plugin-svgr 플러그인 추가
    - TS와 Vite에서 alias 경로를 동일하게 맞춤
    - SVG React 컴포넌트 사용을 위해 vite-env.d.ts 타입 설정 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite';
+import path from "path";
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
+
+  resolve: {
+    alias: [{
+      find: "@",
+      replacement: path.resolve(__dirname, "src")
+    },
+    {
+      find: "@app",
+      replacement: path.resolve(__dirname, "src/app")
+    },
+    {
+      find: "@features",
+      replacement: path.resolve(__dirname, "src/features")
+    },
+    {
+      find: "@pages",
+      replacement: path.resolve(__dirname, "src/pages")
+    },
+    {
+      find: "@shared",
+      replacement: path.resolve(__dirname, "src/shared")
+    },
+    ]
+  }
 });
