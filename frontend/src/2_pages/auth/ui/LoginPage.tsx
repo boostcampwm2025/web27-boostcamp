049103da61ec7e833369e45c7ee752ef5cdaa7e3
kitae <gitae9999@naver.com>
2026-01-13 19:27:19 +0900

    feat: LoginPage 레이아웃 구현

--- frontend/src/2_pages/auth/ui/LoginPage.tsx
+++ frontend/src/2_pages/auth/ui/LoginPage.tsx
@@ -1,6 +1,14 @@
-export function LoginPage(){
-    return(
-    <>
-    </>
-    )
+import { Modal } from "@/4_shared/ui/Modal";
+
+export function LoginPage() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center bg-[#F6F6F8]">
+      <Modal height="h-[800px]">
+        <div className="flex items-center justify-center text-sm ">
+          <p className="text-[#616E89]">계정이 없나요?</p>
+          <p>회원가입</p>
+        </div>
+      </Modal>
+    </div>
+  );
 }
