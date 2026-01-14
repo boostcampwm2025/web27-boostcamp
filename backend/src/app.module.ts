7ab77d269e7362c5cb29fe8b8d06950f916bfb0a
tomass <dominanthat@gmail.com>
2026-01-15 05:59:36 +0900

    feat: appModule에 TypeORM 연결 비동기 설정 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getTypeOrmConfig } from './config/typeorm.config';
+
 import { RTBModule } from './rtb/rtb.module';
 import { BidLogModule } from './bid-log/bid-log.module';
 import { SdkModule } from './sdk/sdk.module';
@@ -11,8 +14,16 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true }), // .env 파일을 전역 설정으로 사용
     RTBModule,
+
+    // 팩토리 함수 사용해서 typeORM 설정 비동기 로드
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        getTypeOrmConfig(configService),
+    }),
+
     BidLogModule,
     SdkModule,
     AdvertiserModule,
