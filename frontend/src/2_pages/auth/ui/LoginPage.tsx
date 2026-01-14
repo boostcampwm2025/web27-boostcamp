d822e656ccced6d5e508c89fdf54c25b846b1a25
kitae <gitae9999@naver.com>
2026-01-14 13:12:35 +0900

    feat: 회원가입,로그인 페이지간 상호 Link 추가

--- frontend/src/2_pages/auth/ui/LoginPage.tsx
+++ frontend/src/2_pages/auth/ui/LoginPage.tsx
@@ -1,14 +1,15 @@
-import { Modal } from "@/4_shared/ui/Modal";
-import { LoginForm } from "../../../3_features/authorize/login/ui/LoginForm";
+import { Modal } from '@/4_shared/ui/Modal';
+import { LoginForm } from '../../../3_features/authorize/login/ui/LoginForm';
+import { Link } from 'react-router-dom';
 
 export function LoginPage() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-[#F6F6F8]">
       <Modal height="h-[700px]">
         <LoginForm />
-        <div className="flex items-center justify-center text-sm ">
+        <div className="flex items-center justify-center text-sm gap-1 ">
           <p className="text-[#616E89]">계정이 없나요?</p>
-          <p>회원가입</p>
+          <Link to={'/auth/register'}>회원가입</Link>
         </div>
       </Modal>
     </div>
--- frontend/src/2_pages/auth/ui/RegisterPage.tsx
+++ frontend/src/2_pages/auth/ui/RegisterPage.tsx
@@ -1,14 +1,15 @@
 import { Modal } from '@shared/ui/Modal';
 import { RegisterForm } from '../../../3_features/authorize/register/ui/RegisterForm';
+import { Link } from 'react-router-dom';
 
 export function RegisterPage() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-[#F6F6F8]">
       <Modal height="h-[900px]">
         <RegisterForm />
-        <div className="flex items-center justify-center text-sm ">
+        <div className="flex items-center justify-center text-sm gap-1 ">
           <p className="text-[#616E89]">이미 계정이 있나요?</p>
-          <p>로그인</p>
+          <Link to={'/auth/login'}>로그인</Link>
         </div>
       </Modal>
     </div>
