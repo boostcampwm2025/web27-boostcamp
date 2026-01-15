edd570b10d268b5636700d2f4c76b5e167a78fdf
kitae <gitae9999@naver.com>
2026-01-15 14:47:03 +0900

    chore: 변수 컨벤션 카멜케이스로 통일
    - /rtb/types/decision.type.ts 의 Campaign Inteface 삭제하고 campaign 모듈 내 타입선언 파일을 import해서 사용

--- backend/src/rtb/types/decision.types.ts
+++ backend/src/rtb/types/decision.types.ts
@@ -1,3 +1,5 @@
+import { Campaign } from 'src/campaign/types/campaign.types';
+
 export interface DecisionContext {
   blogKey: string;
   tags: string[];
@@ -9,25 +11,6 @@ export interface DecisionContext {
 export interface Tag {
   id: number;
   name: string;
-}
-
-export interface Campaign {
-  id: string;
-  user_id: number;
-  title: string;
-  content: string;
-  image: string;
-  url: string;
-  tags: Tag[];
-  max_cpc: number;
-  daily_budget: number;
-  total_budget: number | null;
-  is_high_intent: boolean;
-  status: 'ACTIVE' | 'PAUSED' | 'PENDING' | 'ENDED';
-  start_date: string;
-  end_date: string;
-  created_at: string;
-  deleted_at: string | null;
 }
 
 export interface Candidate {
