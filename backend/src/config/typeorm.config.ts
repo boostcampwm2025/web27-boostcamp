2f8806a2c8766aabe8badcebf13fcda447e31b1c
tomass <dominanthat@gmail.com>
2026-01-15 14:28:55 +0900

    fix: typeORM synchronize설정 변경

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -11,9 +11,9 @@ export const getTypeOrmConfig = (
   password: configService.get<string>('DB_PASSWORD', ''),
   database: configService.get<string>('DB_DATABASE', 'database'),
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  // synchronize: configService.get<string>('NODE_ENV') !== 'production', // 개발 시에만 자동 동기화
+  synchronize: configService.get<string>('NODE_ENV') !== 'production', // 개발 시에만 자동 동기화
   // synchronize: true, // 개발 시에만 자동 동기화
-  synchronize: false,
+  // synchronize: false,
   logging: configService.get<string>('NODE_ENV') === 'development',
   timezone: '+09:00', // KST
 });
