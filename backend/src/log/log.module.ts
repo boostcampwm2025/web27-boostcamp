c4075d293ad3aef864fcdd5e5b2c7aa259762e40
tomass22 <dominanthat@gmail.com>
2026-01-20 01:35:11 +0900

    feat: log에 대한 typeorm 코드 구현 및 모듈에 등록

--- backend/src/log/log.module.ts
+++ backend/src/log/log.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { LogRepository } from './repository/log.repository';
-import { InMemoryLogRepository } from './repository/in-memory-log.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LogRepository } from './repository/log.repository.interface';
+// import { InMemoryLogRepository } from './repository/in-memory-log.repository';
+import { TypeOrmLogRepository } from './repository/typeorm-log.repository';
+import { ViewLogEntity } from './entities/view-log.entity';
+import { ClickLogEntity } from './entities/click-log.entity';
 
 @Module({
-  providers: [{ provide: LogRepository, useClass: InMemoryLogRepository }],
+  imports: [TypeOrmModule.forFeature([ViewLogEntity, ClickLogEntity])],
+  providers: [{ provide: LogRepository, useClass: TypeOrmLogRepository }],
   exports: [LogRepository],
 })
 export class LogModule {}
