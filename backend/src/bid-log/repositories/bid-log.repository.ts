b1afcd67dda021f4cf233c116b23192fe30a0d65
tomass22 <dominanthat@gmail.com>
2026-01-19 22:23:55 +0900

    fix: bid-log 레포의 interface 반환타입 수정
    - 일반 반환 타입에서 Promise객체 형식으로 수정

--- backend/src/bid-log/repositories/bid-log.repository.ts
+++ backend/src/bid-log/repositories/bid-log.repository.ts
@@ -1,11 +1,11 @@
 import { BidLog } from '../bid-log.types';
 
 export abstract class BidLogRepository {
-  abstract save(bidLog: BidLog): void;
-  abstract saveMany(bidLogs: BidLog[]): void;
-  abstract findByAuctionId(auctionId: string): BidLog[];
-  abstract findByCampaignId(campaignId: string): BidLog[];
-  abstract findWinAmountByAuctionId(auctionId: string): number | null;
-  abstract count(): number;
-  abstract getAll(): BidLog[];
+  abstract save(bidLog: BidLog): Promise<void>;
+  abstract saveMany(bidLogs: BidLog[]): Promise<void>;
+  abstract findByAuctionId(auctionId: string): Promise<BidLog[]>;
+  abstract findByCampaignId(campaignId: string): Promise<BidLog[]>;
+  abstract findWinAmountByAuctionId(auctionId: string): Promise<number | null>;
+  abstract count(): Promise<number>;
+  abstract getAll(): Promise<BidLog[]>;
 }
