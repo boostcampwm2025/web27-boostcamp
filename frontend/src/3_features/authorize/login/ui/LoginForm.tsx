cebcc879d41b603c92dd9303e90b18d6e6ed92ad
kitae <gitae9999@naver.com>
2026-01-20 00:10:55 +0900

    feat: 로그인 폼 google 로그인 핸들러 추가

--- frontend/src/3_features/authorize/login/ui/LoginForm.tsx
+++ frontend/src/3_features/authorize/login/ui/LoginForm.tsx
@@ -1,8 +1,14 @@
 import { FormDivider } from '@/4_shared/ui/Divider';
 import { Icon } from '@/4_shared/ui/Icon';
 import { TextField } from '@shared/ui/TextField';
+import { useCallback } from 'react';
+import { handleOauth } from '../../lib/handleOauth';
 
 export function LoginForm() {
+  const onClick = useCallback(() => {
+    handleOauth();
+  },[]);
+
   return (
     <form className="flex flex-col gap-6 mt-15 mx-10">
       <div className="flex flex-col gap-1">
@@ -11,7 +17,11 @@ export function LoginForm() {
           광고주 또는 퍼블리셔 계정으로 로그인하세요.
         </p>
       </div>
-      <button type='button' className="flex h-11 items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white text-base font-semibold text-[#111318] hover:bg-gray-50">
+      <button
+        type="button"
+        className="flex h-11 items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white text-base font-semibold text-[#111318] hover:bg-gray-50"
+        onClick={()=>onClick()}
+      >
         <Icon.Google />
         <span className="cursor-default">Google로 계속하기</span>
       </button>
