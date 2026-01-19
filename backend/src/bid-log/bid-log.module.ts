6f87d37416a5a5acb4b7307184bb3f2016efb247
tomass22 <dominanthat@gmail.com>
2026-01-20 01:30:01 +0900

    feat: bid-log에 Repo<BidLogEntity> 프로바이더 import 추가

--- backend/src/bid-log/bid-log.module.ts
+++ backend/src/bid-log/bid-log.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BidLogController } from './bid-log.controller';
 import { BidLogService } from './bid-log.service';
 import { BidLogRepository } from './repositories/bid-log.repository.interface';
 // import { InMemoryBidLogRepository } from './repositories/in-memory-bid-log.repository';
 import { TypeOrmBidLogRepository } from './repositories/typeorm-bid-log.repository';
+import { BidLogEntity } from './entities/bid-log.entity';
 import { CampaignModule } from 'src/campaign/campaign.module';
 
 @Module({
-  imports: [CampaignModule],
+  imports: [TypeOrmModule.forFeature([BidLogEntity]), CampaignModule],
   controllers: [BidLogController],
   providers: [
     BidLogService,
