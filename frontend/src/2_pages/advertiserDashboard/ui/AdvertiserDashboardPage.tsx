1ec91adc022e821910297fefe616e9d10be3bc8f
2seb2 <yuo231@naver.com>
2026-01-20 01:15:20 +0900

    feat: 광고주 대시보드에 캠페인 테이블 추가
    - CampaignStatsTable 컴포넌트 import 및 렌더링

--- frontend/src/2_pages/advertiserDashboard/ui/AdvertiserDashboardPage.tsx
+++ frontend/src/2_pages/advertiserDashboard/ui/AdvertiserDashboardPage.tsx
@@ -1,10 +1,12 @@
 import { AccountSummaryCardList } from '@features/accountSummary';
+import { CampaignStatsTable } from '@features/campaignStats';
 import { RealtimeBidsTable } from '@features/realtimeBids';
 
 export function AdvertiserDashboardPage() {
   return (
     <div className="flex flex-col gap-4 px-8 py-8 bg-gray-100">
       <AccountSummaryCardList />
+      <CampaignStatsTable />
       <RealtimeBidsTable />
     </div>
   );
