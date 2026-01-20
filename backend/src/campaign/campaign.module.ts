889ced05ab24d421d901467743fa61a53e9931c4
chazicer <my0710my@gmail.com>
2026-01-21 00:35:19 +0900

    feat: 캠페인 모듈러 수정
    - campaign.service
    - campaign.cronservice를 추가한 것을 유의.

--- backend/src/campaign/campaign.module.ts
+++ backend/src/campaign/campaign.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CampaignRepository } from './repository/campaign.repository';
 import { JsonCampaignRepository } from './repository/json-campaign.repository';
+import { CampaignService } from './campaign.service';
+import { CampaignController } from './campaign.controller';
+import { CampaignCronService } from './campaign-cron.service';
 
 @Module({
+  controllers: [CampaignController],
   providers: [
+    CampaignService,
+    CampaignCronService,
     { provide: CampaignRepository, useClass: JsonCampaignRepository },
   ],
   exports: [CampaignRepository],
