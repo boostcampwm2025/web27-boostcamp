4d5f06daf82792f16700fc59be433dec1ce11cf6
kitae <gitae9999@naver.com>
2026-01-21 16:50:53 +0900

    chore: User 컨트롤러 추가

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserRepository } from './repository/user.repository';
 import { TypeOrmUserRepository } from './repository/typeorm-user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
-// import { JsonUserRepository } from './repository/user/json-user.repository';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [{ provide: UserRepository, useClass: TypeOrmUserRepository }],
   exports: [UserRepository],
+  controllers: [UserController],
 })
 export class UserModule {}
