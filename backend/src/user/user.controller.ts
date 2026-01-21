c84baa2b85cdf481a46b97ac708d87d4767942b9
kitae <gitae9999@naver.com>
2026-01-21 17:13:56 +0900

    feat: User Repository 최초 로그인필드 업데이트 로직 추가

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Post, Req } from '@nestjs/common';
 import { type AuthenticatedRequest } from 'src/types/authenticated-request';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post('me/first-login')
-  checkFirstLogin(@Req() req: AuthenticatedRequest) {}
+  async handleFirstLogin(@Req() req: AuthenticatedRequest) {
+    const { userId } = req.user;
+    await this.userService.handleFirstLogin(userId);
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(UserEntity) private readonly userRepo: Repository<UserEntity>) {}
+
+  async handleFirstLogin(userId: number) {
+    const isFirstLogin = this.userRepo.
+  }
+}
