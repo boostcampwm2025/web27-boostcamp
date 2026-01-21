60ef8410ec43156df444f7daa7118a326cbcee64
tomass22 <dominanthat@gmail.com>
2026-01-22 00:00:40 +0900

    feat: cache 모듈에 Redis설정 추가

--- backend/src/cache/cache.module.ts
+++ backend/src/cache/cache.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AuctionStore } from './auction/auction.store';
-import { InMemoryAuctionStore } from './auction/in-memory-auction.store';
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
 import { CacheController } from './cache.controller';
 import { LogModule } from 'src/log/log.module';
+import { RedisCacheConfig } from 'src/config/redis.config';
+import { AuctionStore } from './auction/auction.store';
+import { RedisAuctionStore } from './auction/redis-auction.store';
 
 @Module({
-  imports: [LogModule],
+  imports: [
+    LogModule,
+    // TODO: 추후에 app모듈로 위치 변경 예정
+    NestCacheModule.registerAsync({
+      isGlobal: true,
+      useClass: RedisCacheConfig,
+    }),
+  ],
   controllers: [CacheController],
-  providers: [{ provide: AuctionStore, useClass: InMemoryAuctionStore }],
+  providers: [{ provide: AuctionStore, useClass: RedisAuctionStore }],
   exports: [AuctionStore],
 })
 export class CacheModule {}
