d6f5ba0f2632fa58f9482f7fa9a311ce29071c1b
tomass22 <dominanthat@gmail.com>
2026-01-26 02:21:55 +0900

    feat: redis 라이브러리 세팅 변경 -> cache-manager-ioredis

--- backend/src/cache/cache.module.ts
+++ backend/src/cache/cache.module.ts
@@ -1,21 +1,40 @@
 import { Module } from '@nestjs/common';
 import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
 import { LogModule } from 'src/log/log.module';
-import { RedisCacheConfig } from 'src/config/redis.config';
+import { RedisModule, REDIS_CLIENT } from 'src/redis/redis.module';
 import { CacheRepository } from './repository/cache.repository.interface';
 import { RedisCacheRepository } from './repository/redis-cache.repository';
+import Redis from 'ioredis';
+import { CampaignCacheService } from './services/campaign-cache.service';
+import { BlogCacheService } from './services/blog-cache.service';
+import { RedisIndexService } from './services/redis-index.service';
 
 @Module({
   imports: [
     LogModule,
-    // TODO(후순위): 추후에 app모듈로 위치 변경 예정
+    RedisModule,
     NestCacheModule.registerAsync({
       isGlobal: true,
-      useClass: RedisCacheConfig,
+      imports: [RedisModule],
+      useFactory: async (redisClient: Redis) => {
+        return {
+          store: await import('cache-manager-ioredis').then(
+            (m) => m.default || m
+          ),
+          redisInstance: redisClient,
+          ttl: 5, // 기본 TTL 5초 (cache-manager-ioredis는 초 단위 사용)
+        };
+      },
+      inject: [REDIS_CLIENT],
     }),
   ],
   controllers: [],
-  providers: [{ provide: CacheRepository, useClass: RedisCacheRepository }],
-  exports: [CacheRepository],
+  providers: [
+    { provide: CacheRepository, useClass: RedisCacheRepository },
+    CampaignCacheService,
+    BlogCacheService,
+    RedisIndexService,
+  ],
+  exports: [CacheRepository, CampaignCacheService, BlogCacheService],
 })
 export class CacheModule {}
