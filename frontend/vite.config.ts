2f43ab138d34bf765fc565466342d5fecab2be12
tomass22 <dominanthat@gmail.com>
2026-01-27 02:46:15 +0900

    fix: https를 local에도 적용에 따른 설정 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,34 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import fs from 'fs';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+
+  server: isDev
+    ? {
+        https: {
+          key: fs.readFileSync(
+            path.resolve(__dirname, '../certs/localhost-key.pem')
+          ),
+          cert: fs.readFileSync(
+            path.resolve(__dirname, '../certs/localhost.pem')
+          ),
+        },
+        port: 5173,
+        proxy: {
+          '/api': 'http://localhost:3000',
+        },
+      }
+    : {
+        port: 5173,
+      },
 
   resolve: {
     alias: [
