7e18f2ef12e36a9bd042709f69708ab4d556aa4e
2seb2 <yuo231@naver.com>
2026-01-27 06:04:43 +0900

    feat: 캠페인 상세 관련 타입 및 진입점 추가
    - 캠페인 상세 페이지에서 사용될 타입 정의
    - 해당 기능들을 모듈 외부로 노출하기 위한 index.ts 파일을 추가

---
+++ frontend/src/3_features/campaignDetail/index.ts
@@ -0,0 +1,15 @@
+export { useCampaignDetail } from './lib/useCampaignDetail';
+export { useUpdateCampaign } from './lib/useUpdateCampaign';
+export { useUpdateBudget } from './lib/useUpdateBudget';
+export { usePauseCampaign } from './lib/usePauseCampaign';
+export { useSpendingLog } from './lib/useSpendingLog';
+
+export type {
+  CampaignStatus,
+  CampaignDetail,
+  Tag,
+  UpdateCampaignRequest,
+  UpdateBudgetRequest,
+  SpendingLogItem,
+  SpendingLogResponse,
+} from './lib/types';
