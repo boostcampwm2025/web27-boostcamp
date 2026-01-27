4eff4f9e496e7b077350dc151d4e939c93cdef64
chazicer <my0710my@gmail.com>
2026-01-27 15:51:48 +0900

    feat: 레이아웃 변경 및 속성명 변경

--- frontend/src/3_features/creditBalance/index.ts
+++ frontend/src/3_features/creditBalance/index.ts
@@ -1,3 +1,2 @@
 export { CreditBalanceCard } from './ui/CreditBalanceCard';
 export { ChargeAmountSelector } from './ui/ChargeAmountSelector';
-export { useCreditBalance } from './lib/useCreditBalance';
