eccafac1e2589f16d7840169b2c7c3b91f9dfb77
tomass22 <dominanthat@gmail.com>
2026-01-27 18:12:47 +0900

    fix: cache모듈 팀원 구현 방식에 맞게 교환

--- backend/src/cache/cache.module.ts
+++ backend/src/cache/cache.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
 import { LogModule } from 'src/log/log.module';
-import { RedisModule, REDIS_CLIENT } from 'src/redis/redis.module';
 import { RedisCacheConfig } from 'src/config/redis.config';
 import { RedisModule } from 'src/redis/redis.module';
 import { CacheRepository } from './repository/cache.repository.interface';
 import { RedisCacheRepository } from './repository/redis-cache.repository';
-import Redis from 'ioredis';
-import { BlogCacheService } from './services/blog-cache.service';
 import { RedisIndexService } from './services/redis-index.service';
-
-// TODO: 맞는 방식이었는지 점검 필요
-// eslint검증 피하기 위한 cache-manager-ioredis 타입 정의
-type RedisStoreModule = {
-  default?: unknown;
-  [key: string]: unknown;
-};
 
 @Module({
   imports: [
@@ -31,9 +21,8 @@ type RedisStoreModule = {
   controllers: [],
   providers: [
     { provide: CacheRepository, useClass: RedisCacheRepository },
-    BlogCacheService,
     RedisIndexService,
   ],
-  exports: [CacheRepository, BlogCacheService],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
