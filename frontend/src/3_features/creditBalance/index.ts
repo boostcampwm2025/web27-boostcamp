ccd0ca182e0910aeb6a8a6be939368cf0f235644
chazicer <my0710my@gmail.com>
2026-01-27 09:22:40 +0900

    feat: 현재 유저의 크레딧 정보 가져오기 추가
    - balance를 추가로 가져옴

--- frontend/src/3_features/creditBalance/index.ts
+++ frontend/src/3_features/creditBalance/index.ts
@@ -1,2 +1,3 @@
 export { CreditBalanceCard } from './ui/CreditBalanceCard';
 export { ChargeAmountSelector } from './ui/ChargeAmountSelector';
+export { useCreditBalance } from './lib/useCreditBalance';
