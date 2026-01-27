b2038a436b92bdbbd6b8365df26152283044b8e8
tomass22 <dominanthat@gmail.com>
2026-01-27 19:10:32 +0900

    feat: redis클라이언트 관련 타입 추가

--- backend/src/redis/redis.constant.ts
+++ backend/src/redis/redis.constant.ts
@@ -1,1 +1,2 @@
 export const REDIS_CLIENT = Symbol('REDIS_CLIENT');
+export const REDIS_JSON_CLIENT = Symbol('REDIS_JSON_CLIENT');
--- backend/src/redis/redis.type.ts
+++ backend/src/redis/redis.type.ts
@@ -1,3 +1,5 @@
 import type { RedisClientType } from '@keyv/redis';
+import type { Redis } from 'ioredis';
 
 export type AppRedisClient = RedisClientType;
+export type AppRedisJsonClient = Redis;
