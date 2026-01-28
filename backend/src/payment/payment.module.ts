0d4f4b51ff18531fefaceeab10799ed23d64a80c
chazicer <my0710my@gmail.com>
2026-01-29 06:37:26 +0900

    feat: payment 모듈에서 모듈 등록 및 유저 모듈 의존 제거

--- backend/src/payment/payment.module.ts
+++ backend/src/payment/payment.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
-import { UserModule } from '../user/user.module';
+import { AdvertiserModule } from '../advertiser/advertiser.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [AdvertiserModule],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
