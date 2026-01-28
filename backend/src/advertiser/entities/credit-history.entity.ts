840e440028f51d69af5bb447587dc36fe8320649
chazicer <my0710my@gmail.com>
2026-01-29 06:29:04 +0900

    feat: 엔티티, dto, 리포지토리도 이동

---
+++ backend/src/advertiser/entities/credit-history.entity.ts
@@ -0,0 +1,70 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { UserEntity } from '../../user/entities/user.entity';
+import { CampaignEntity } from '../../campaign/entities/campaign.entity';
+
+export enum CreditHistoryType {
+  CHARGE = 'CHARGE',
+  WITHDRAW = 'WITHDRAW',
+}
+
+@Entity('CreditHistory')
+export class CreditHistoryEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'user_id', type: 'int' })
+  userId: number;
+
+  @Column({
+    type: 'enum',
+    enum: CreditHistoryType,
+    comment: 'CHARGE: 충전, WITHDRAW: 캠페인 생성 시 차감',
+  })
+  type: CreditHistoryType;
+
+  @Column({ type: 'int', comment: '변동 금액 (절댓값)' })
+  amount: number;
+
+  @Column({
+    name: 'balance_after',
+    type: 'int',
+    comment: '변동 후 잔액',
+  })
+  balanceAfter: number;
+
+  @Column({
+    name: 'campaign_id',
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+    comment: 'WITHDRAW인 경우 생성된 캠페인 ID',
+  })
+  campaignId: string | null;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+    comment: '충전/차감 설명 (예: 신규 가입 축하 크레딧)',
+  })
+  description: string | null;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  // Relations
+  @ManyToOne(() => UserEntity, (user) => user.creditHistories)
+  @JoinColumn({ name: 'user_id' })
+  user: UserEntity;
+
+  @ManyToOne(() => CampaignEntity, { nullable: true })
+  @JoinColumn({ name: 'campaign_id' })
+  campaign: CampaignEntity | null;
+}
