ce57b7f20de2b396b1626bad2663516ad15446e3
tomass22 <dominanthat@gmail.com>
2026-02-03 01:42:26 +0900

    feat: cache모듈에 import 추가

--- backend/src/cache/cache.module.ts
+++ backend/src/cache/cache.module.ts
@@ -3,11 +3,16 @@ import { LogModule } from 'src/log/log.module';
 import { RedisModule } from 'src/redis/redis.module';
 import { CacheRepository } from './repository/cache.repository.interface';
 import { RedisCacheRepository } from './repository/redis-cache.repository';
+import { RedisTTLWorker } from './redis-ttl.worker';
+import { CampaignModule } from 'src/campaign/campaign.module';
 
 @Module({
-  imports: [LogModule, RedisModule],
+  imports: [LogModule, RedisModule, CampaignModule],
   controllers: [],
-  providers: [{ provide: CacheRepository, useClass: RedisCacheRepository }],
+  providers: [
+    { provide: CacheRepository, useClass: RedisCacheRepository },
+    RedisTTLWorker,
+  ],
   exports: [CacheRepository],
 })
 export class CacheModule {}
