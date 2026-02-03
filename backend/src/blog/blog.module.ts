eba54bf8ee59a70f1c41fb505b526aea990ef7d3
kitae <gitae9999@naver.com>
2026-02-04 01:38:35 +0900

    refactort: blogModule 큐등록제거

--- backend/src/blog/blog.module.ts
+++ backend/src/blog/blog.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BullModule } from '@nestjs/bullmq';
 import { BlogEntity } from './entities/blog.entity';
 import { UserModule } from 'src/user/user.module';
 import { BlogRepository } from './repository/blog.repository.interface';
@@ -10,15 +9,14 @@ import { TypeOrmBlogRepository } from './repository/typeorm-blog.repository';
 import { RedisModule } from 'src/redis/redis.module';
 import { BlogRedisCacheRepository } from './repository/redis-blog.cache.repository';
 import { BlogCacheRepository } from './repository/blog.cache.repository.interface';
+import { QueueModule } from 'src/queue/queue.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([BlogEntity]),
     UserModule,
     RedisModule,
-    BullModule.registerQueue({
-      name: 'embedding-queue',
-    }),
+    QueueModule,
   ],
   controllers: [BlogController],
   providers: [
