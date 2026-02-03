46a978a8c5d9880e0ce89a18f2f9e6daabc7cc0f
kitae <gitae9999@naver.com>
2026-02-03 13:19:25 +0900

    feat: metrics모듈에 interceptor 등록

--- backend/src/metrics/metrics.module.ts
+++ backend/src/metrics/metrics.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from './metrics.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpMetricsInterceptor } from './http-metrics.interceptor';
 
 @Module({
   controllers: [MetricsController],
-  providers: [MetricsService],
+  providers: [
+    MetricsService,
+    { provide: APP_INTERCEPTOR, useClass: HttpMetricsInterceptor },
+  ],
 })
 export class MetricsModule {}
