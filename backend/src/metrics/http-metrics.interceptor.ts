78368f244f8062e453d8d3a8ea8b64d75c1191b7
kitae <gitae9999@naver.com>
2026-02-03 15:39:41 +0900

    feat: httpmetricsinterceptor implements

--- backend/src/metrics/http-metrics.interceptor.ts
+++ backend/src/metrics/http-metrics.interceptor.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class HttpMetricsInterceptor {}
+export class HttpMetricsInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>
+  ): Observable<any> | Promise<Observable<any>> {
+    throw new Error('Method not implemented.');
+  }
+}
