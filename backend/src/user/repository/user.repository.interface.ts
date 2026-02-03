2713958d0d7a3eae58c56e5b5bab4fe8f8b8b186
chazicer <my0710my@gmail.com>
2026-02-03 18:07:14 +0900

    feat: createuser시 termsAgreedAt추가 리포지토리 수정

--- backend/src/user/repository/user.repository.interface.ts
+++ backend/src/user/repository/user.repository.interface.ts
@@ -3,7 +3,11 @@ import { UserEntity, UserRole } from 'src/user/entities/user.entity';
 export abstract class UserRepository {
   abstract getById(userId: number): Promise<UserEntity | null>;
   abstract verifyRole(userId: number, role: UserRole): Promise<boolean>;
-  abstract createUser(email: string, role: UserRole): Promise<number>;
+  abstract createUser(
+    email: string,
+    role: UserRole,
+    termsAgreedAt?: Date
+  ): Promise<number>;
   abstract findByEmail(email: string): Promise<number | null>;
   abstract setFirstLoginAtIfNull(userId: number): Promise<boolean>;
   abstract getBalanceById(userId: number): Promise<number | null>;
