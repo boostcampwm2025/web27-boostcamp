bf59ca07db465e6a597adcfba13cac854f68adca
kitae <gitae9999@naver.com>
2026-02-04 16:37:19 +0900

    chore: worker모듈 프로바이더 추가

--- backend/src/worker/worker.module.ts
+++ backend/src/worker/worker.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { RedisCacheRepository } from 'src/cache/repository/redis-cache.repository';
+import { CampaignCacheRepository } from 'src/campaign/repository/campaign.cache.repository.interface';
 import { QueueModule } from 'src/queue/queue.module';
 import { EmbeddingWorker } from 'src/queue/workers/embedding.worker';
 import { RedisModule } from 'src/redis/redis.module';
+import { MLEngine } from 'src/rtb/ml/mlEngine.interface';
+import { XenovaMLEngine } from 'src/rtb/ml/xenova-mlEngine';
 
 @Module({
   imports: [
@@ -12,6 +16,10 @@ import { RedisModule } from 'src/redis/redis.module';
     RedisModule,
     QueueModule,
   ],
-  providers: [EmbeddingWorker],
+  providers: [
+    EmbeddingWorker,
+    { provide: MLEngine, useClass: XenovaMLEngine },
+    { provide: CampaignCacheRepository, useClass: RedisCacheRepository },
+  ],
 })
 export class WorkerModule {}
