6fc89b186276c01bf497328585829dda438d0c79
kitae <gitae9999@naver.com>
2026-02-04 16:52:38 +0900

    chore: 모듈 순환 참조 제거

--- backend/src/queue/queue.module.ts
+++ backend/src/queue/queue.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { RTBModule } from 'src/rtb/rtb.module';
-import { CampaignModule } from 'src/campaign/campaign.module';
-import { BlogModule } from 'src/blog/blog.module';
 
 @Module({
   imports: [
     ConfigModule,
-    RTBModule,
-    CampaignModule,
-    BlogModule,
     BullModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
