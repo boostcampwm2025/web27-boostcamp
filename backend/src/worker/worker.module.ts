bbeccfbb385add99ac63094a6060dd9d7277ac68
kitae <gitae9999@naver.com>
2026-02-04 22:07:01 +0900

    refactor: embedding worker 디렉토리 이동

--- backend/src/worker/worker.module.ts
+++ backend/src/worker/worker.module.ts
@@ -4,7 +4,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { CampaignCacheRepository } from 'src/campaign/repository/campaign.cache.repository.interface';
 import { RedisCampaignCacheRepository } from 'src/campaign/repository/redis-campaign.cache.repository';
 import { QueueModule } from 'src/queue/queue.module';
-import { EmbeddingWorker } from 'src/queue/workers/embedding.worker';
+import { EmbeddingWorker } from 'src/worker/embedding.worker';
 import { RedisModule } from 'src/redis/redis.module';
 import { MLEngine } from 'src/rtb/ml/mlEngine.interface';
 import { XenovaMLEngine } from 'src/rtb/ml/xenova-mlEngine';
