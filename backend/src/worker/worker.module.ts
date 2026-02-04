02a8c1f5aaf006840d7c1af0bec8dc35b8cf9038
kitae <gitae9999@naver.com>
2026-02-04 16:53:36 +0900

    chore: Worker 모듈 useclass 수정

--- backend/src/worker/worker.module.ts
+++ backend/src/worker/worker.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { RedisCacheRepository } from 'src/cache/repository/redis-cache.repository';
 import { CampaignCacheRepository } from 'src/campaign/repository/campaign.cache.repository.interface';
+import { RedisCampaignCacheRepository } from 'src/campaign/repository/redis-campaign.cache.repository';
 import { QueueModule } from 'src/queue/queue.module';
 import { EmbeddingWorker } from 'src/queue/workers/embedding.worker';
 import { RedisModule } from 'src/redis/redis.module';
@@ -19,7 +19,10 @@ import { XenovaMLEngine } from 'src/rtb/ml/xenova-mlEngine';
   providers: [
     EmbeddingWorker,
     { provide: MLEngine, useClass: XenovaMLEngine },
-    { provide: CampaignCacheRepository, useClass: RedisCacheRepository },
+    {
+      provide: CampaignCacheRepository,
+      useClass: RedisCampaignCacheRepository,
+    },
   ],
 })
 export class WorkerModule {}
