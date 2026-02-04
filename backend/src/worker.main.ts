6ab00762219465710d46fa4fc3312978f30ce1ae
kitae <gitae9999@naver.com>
2026-02-04 19:16:40 +0900

    chore: worker프로세스 부트스트랩 실패시 에러처리

--- backend/src/worker.main.ts
+++ backend/src/worker.main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { WorkerModule } from './worker/worker.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(WorkerModule);
-  app.enableShutdownHooks();
+  try {
+    const app = await NestFactory.createApplicationContext(WorkerModule);
+    app.enableShutdownHooks();
+    console.log('Worker 부트스트랩 성공');
+  } catch (error) {
+    console.error('Worker 부트스트랩 실패:', error);
+    process.exit(1);
+  }
 }
 
 void bootstrap();
