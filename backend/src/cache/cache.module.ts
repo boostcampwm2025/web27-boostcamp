c25bad9ae0ecf256a305320e9b9b0d793f1ed30d
kitae <gitae9999@naver.com>
2026-02-05 10:02:35 +0900

    refactor: cacheModule campaignmodule 의존성 제거

--- backend/src/cache/cache.module.ts
+++ backend/src/cache/cache.module.ts
@@ -4,10 +4,10 @@ import { RedisModule } from 'src/redis/redis.module';
 import { CacheRepository } from './repository/cache.repository.interface';
 import { RedisCacheRepository } from './repository/redis-cache.repository';
 // import { RedisTTLWorker } from '../worker/redis-ttl.worker';
-import { CampaignModule } from 'src/campaign/campaign.module';
+// import { CampaignModule } from 'src/campaign/campaign.module';
 
 @Module({
-  imports: [LogModule, RedisModule, CampaignModule],
+  imports: [LogModule, RedisModule],
   providers: [
     { provide: CacheRepository, useClass: RedisCacheRepository },
     // RedisTTLWorker,
